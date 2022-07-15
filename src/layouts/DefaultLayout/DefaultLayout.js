450fef5180f91d93daa1a12afa070571ab5b343d
NGUYEN HOI <nguyenhoi13041999@gmail.com>
2022-07-15 23:50:05 +0700

    Xây dựng UI sidebar phần 1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,5 +1,5 @@
 import Header from "~/layouts/components/Header";
-import Sidebar from "./Sidebar/Sidebar";
+import Sidebar from '~/layouts/components/Sidebar';
 import styles from "./DefaultLayout.module.scss"
 import classNames from 'classnames/bind';
 import PropTypes from "prop-types";
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,7 @@ import Following from '~/pages/Following'
 import Profile from '~/pages/Profile'
 import Upload from '~/pages/Upload'
 import Search from '~/pages/Search'
+import Live from '~/pages/Live'
 
 
 
@@ -14,8 +15,10 @@ const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
     { path: config.routes.profile, component: Profile },
+    { path: config.routes.live, component: Live },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
+
 ]
 
 const privateRoutes = [
